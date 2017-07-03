03d2bcd2746709d8b9ecc13518f5079269f41474
Dmytro.Denysov@racingpost.com <Dmytro.Denysov@racingpost.com>
2017-07-03 19:54:34 +0300

    Login + Immutable JS improvements

--- src/php/src/ApiBundle/Form/UserType.php
+++ src/php/src/ApiBundle/Form/UserType.php
@@ -3,6 +3,7 @@
 namespace ApiBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,19 @@ class UserType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('first_name')->add('last_name')->add('email')->add('password')->add('gender')->add('isActive');
+        $builder->add('first_name')
+            ->add('last_name')
+            ->add('email')
+            ->add('password')
+            ->add('gender', ChoiceType::class, array(
+                'choices'  => array(
+                    'Maybe' => null,
+                    'Yes' => true,
+                    'No' => false,
+                )))
+            ->add('isActive');
     }
-    
+
     /**
      * {@inheritdoc}
      */
