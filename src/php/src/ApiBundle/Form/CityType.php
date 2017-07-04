1853d69a919a4c6bf22b435670615ad08dedb240
Dmytro.Denysov@racingpost.com <Dmytro.Denysov@racingpost.com>
2017-07-04 16:47:57 +0300

    Bugfix

---
+++ src/php/src/ApiBundle/Form/CityType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ApiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CityType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('lat')->add('lon')->add('region_id')->add('region');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ApiBundle\Entity\City'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'apibundle_city';
+    }
+
+
+}
